6e3c19f467cd0ea50890fc6d83fd35df314a7e9d
danyil <danichka940@gmail.com>
2024-01-05 09:47:24 +0200

    add info dto

---
+++ Models/InfoDTO.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace LolStats742.Models
+{
+    /// <summary>
+    /// Contains detailed game information.
+    /// </summary>\
+    public class InfoDTO
+    {
+        /// <summary>
+        /// Gets or sets the game creation time in UTC. Designates when the team select lobby is created and/or the game is made through match making; not when the game actually starts.
+        /// </summary>
+        public long GameCreation { get; set; }
+
+        /// <summary>
+        /// Gets or sets the game duration.
+        /// </summary>
+        /// <remarks>
+        /// The game duration is serialized as an integer representing the duration of the game in seconds.
+        /// </remarks>
+        public TimeSpan GameDuration { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <remarks>
+        /// 
+        /// </remarks>
+        public long GameEndTimestamp { get; set; }
+
+        /// <summary>
+        /// Gets or sets the ID of the game.
+        /// </summary>
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        public long GameId { get; set; }
+
+        /// <summary>
+        /// Gets or sets the game mode. This should equal one of the <see cref="Models.GameMode"/> values.
+        /// </summary>
+        public string GameMode { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public string GameName { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <remarks>
+        /// 
+        /// </remarks>
+        public long GameStartTimestamp { get; set; }
+
+        /// <summary>
+        /// Gets or sets the game type. This should equal one of the <see cref="Models.GameType"/> values.
+        /// </summary>
+        public string GameType { get; set; }
+
+        /// <summary>
+        /// Gets or sets the game version (patch number).
+        /// </summary>
+        public string GameVersion { get; set; }
+
+        /// <summary>
+        /// Gets or sets the map ID of the map where the match was played.
+        /// </summary>
+        public int MapId { get; set; }
+
+        /// <summary>
+        /// Gets or sets the list of participants' information.
+        /// </summary>
+        public virtual List<MatchParticipant> Participants { get; set; }
+
+        /// <summary>
+        /// Gets or sets the platform ID of the match. This should equal one of the <see cref="Models.PlatformId"/> values.
+        /// </summary>
+        public string PlatformId { get; set; }
+
+        /// <summary>
+        /// Gets or sets the game queue type.
+        /// </summary>
+        public int QueueId { get; set; }
+
+        /// <summary>
+        /// Gets or sets team information.
+        /// </summary>
+        public virtual List<MatchTeam> Teams { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public string TournamentCode { get; set; }
+    }
+}
---
+++ Models/ParticipantDTO.cs
@@ -0,0 +1,67 @@
+﻿using Newtonsoft.Json;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace RiotNet.Models
+{
+    /// <summary>
+    /// This object contains match participant information.
+    /// </summary>
+    public class MatchParticipant
+    {
+        /// <summary>
+        /// Gets or sets champion ID.
+        /// </summary>
+        public int ChampionId { get; set; }
+
+        /// <summary>
+        /// Gets or sets highest ranked tier achieved for the previous season, if any; otherwise null. Used to display border in game loading screen. This should equal one of the <see cref="Tier"/> values.
+        /// </summary>
+        public string HighestAchievedSeasonTier { get; set; }
+
+        /// <summary>
+        /// Gets or sets the match participant ID (normally 1-10).
+        /// </summary>
+        public int ParticipantId { get; set; }
+
+        /// <summary>
+        /// Gets or sets the first summoner spell ID.
+        /// </summary>
+        public int Spell1Id { get; set; }
+
+        /// <summary>
+        /// Gets or sets the second summoner spell ID.
+        /// </summary>
+        public int Spell2Id { get; set; }
+
+        /// <summary>
+        /// Gets or sets the participant statistics.
+        /// </summary>
+        public MatchParticipantStats Stats { get; set; } = new MatchParticipantStats();
+
+        /// <summary>
+        /// Gets or sets the team ID.
+        /// </summary>
+        public TeamSide TeamId { get; set; }
+
+        /// <summary>
+        /// Gets or sets timeline data. Delta fields refer to values for the specified period. Diff fields refer to the deltas versus the calculated lane opponent(s).
+        /// </summary>
+        /// <remarks>
+        /// Delta fields refer to values for the specified period
+        /// (e.g. the gold per minute over the first 10 minutes of the game versus the second 20 minutes of the game).
+        /// Diff fields refer to the deltas versus the calculated lane opponent(s).
+        /// </remarks>
+        public MatchParticipantTimeline Timeline { get; set; } = new MatchParticipantTimeline();
+
+#if DB_READY
+        /// <summary>
+        /// Gets or sets the ID of the current <see cref="MatchParticipant"/>. This does NOT come from the Riot API; it is used as a key when storing this object in a database.
+        /// </summary>
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [JsonIgnore]
+        public long DatabaseId { get; set; }
+#endif
+    }
+}
